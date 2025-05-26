0ee96d2aa12bde1bcca7bb3d32b7d5ebf147dc6f
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-05-26 10:59:33 -0700

    added stats component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaPlay } from 'react-icons/fa';
+import { FaPlay, FaUsers, FaCheckCircle, FaUserFriends } from 'react-icons/fa';
 import heroImage from '../data/hero.png';
 
 const Hero = () => {
@@ -23,6 +23,14 @@ const Hero = () => {
           </div>
         </div>
         <div className="hero__image">
+          <div className="hero__floating-box hero__floating-box--users">
+            <FaUsers className="hero__floating-icon" />
+            <span className="hero__floating-text">15K+ Canadians trust Hiknow</span>
+          </div>
+          <div className="hero__floating-box hero__floating-box--success">
+            <FaCheckCircle className="hero__floating-icon" />
+            <span className="hero__floating-text">Success! Your mood has been recorded</span>
+          </div>
           <img src={heroImage} alt="Hiknow App Preview" />
         </div>
       </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from '../layouts/Layout';
 import Hero from '../components/Hero';
+import StatsSection from '../components/StatsSection';
 
 const Landing = () => {
   return (
     <Layout>
       <div>
         <Hero />
+        <StatsSection />
       </div>
     </Layout>
   );
